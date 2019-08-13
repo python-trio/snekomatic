26dbfd6416606269c5d7c1ecdc1f6ad5743539df
Nathaniel J. Smith <njs@pobox.com>
2019-08-13 01:42:52 -0700

    try this

--- snekbot/__main__.py
+++ snekbot/__main__.py
@@ -1,5 +1,6 @@
 import pprint
 import logging
+import functools
 
 logger = logging.getLogger(__name__)
 
@@ -7,6 +8,19 @@ from octomachinery.app.server.runner import run as run_app
 from octomachinery.app.routing import process_event_actions
 from octomachinery.app.routing.decorators import process_webhook_payload
 from octomachinery.app.runtime.context import RUNTIME_CONTEXT
+
+class dotdict(dict):
+    def __getattr__(self, name):
+        value = self[name]
+        if isinstance(value, dict):
+            return dotdict(value)
+        else:
+            return value
+
+def dotify_payload(fn):
+    @functools.wraps(fn)
+    def wrapper(event):
+        return fn(event.event, dotdict(event.data))
 
 @process_event_actions('issues', {'opened'})
 @process_webhook_payload
@@ -28,13 +42,15 @@ async def on_issue_opened(
     await github_api.post(comments_api_url, data={"body": message})
 
 @process_event_actions('issue_comment', {'created'})
-async def on_issue_comment_created(event):
-    print(event)
-    print(dir(event))
-    print(event.delivery_id)
-    print(event.event)
-    print(event.from_http)
-    print(event.data)
+@dotify_payload
+async def on_issue_comment_created(event, payload):
+    print(f"got a comment on {payload.issue.html_url}")
+    if "pull_request" in payload.issue:
+        print(f"that's a PR: {payload.issue.pull_request.html_url}")
+    else:
+        print("that's not a PR")
+    print(f"user: {payload.comment.user.login!r}")
+    print(f"body: {payload.comment.user.body!r}")
 
 if __name__ == "__main__":
     print("WHEEE")
