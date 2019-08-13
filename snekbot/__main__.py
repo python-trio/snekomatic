94308b41b447a9c073dd60cacba0610ae53e1727
Nathaniel J. Smith <njs@pobox.com>
2019-08-12 23:04:57 -0700

    messing around

--- snekbot/__main__.py
+++ snekbot/__main__.py
@@ -1,4 +1,20 @@
+import pprint
+
 from octomachinery.app.server.runner import run as run_app
+from octomachinery.app.routing import process_event_actions
+from octomachinery.app.routing.decorators import process_webhook_payload
+from octomachinery.app.runtime.context import RUNTIME_CONTEXT
+
+@process_event_actions('issues', {'opened'})
+@process_webhook_payload
+async def on_issue_opened(
+        *,
+        action, issue, repository, sender, installation,
+        assignee=None, changes=None,
+):
+    """Whenever an issue is opened, greet the author and say thanks."""
+    github_api = RUNTIME_CONTEXT.app_installation_client
+    pprint.pprint(locals())
 
 if __name__ == "__main__":
     run_app(
