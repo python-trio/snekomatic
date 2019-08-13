abe3b15493f8258299e53567d8c9a1a9e6823362
Nathaniel J. Smith <njs@pobox.com>
2019-08-12 21:33:51 -0700

    trivial main

---
+++ snekbot/__main__.py
@@ -0,0 +1,8 @@
+from octomachinery.app.server.runner import run as run_app
+
+if __name__ == "__main__":
+    run_app(
+        name='ancient-ocean-35232',
+        version='1.0.0',
+        url='https://github.com/apps/ancient-ocean-35232',
+    )
