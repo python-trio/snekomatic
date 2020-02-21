db36470f0e464cad526598d0382f8610d8d8663e
Nathaniel J. Smith <njs@pobox.com>
2020-02-21 12:59:11 -0800

    Set up alembic for automatic database migrations
    - Auto-migrate on startup
    - We let the auto-migration logic build the schema from an empty db
      during tests
    - We validate that the final db schema matches what our sqlalchemy
      schema says

--- snekomatic/db.py
+++ snekomatic/db.py
@@ -1,27 +1,51 @@
 import os
+from pathlib import Path
 from sqlalchemy import create_engine, MetaData, Table, Column, String
 from sqlalchemy.sql.expression import select, exists
+import alembic.config
+import alembic.command
+import alembic.migration
+import alembic.autogenerate
+import pprint
 import attr
 
 metadata = MetaData()
 
 sent_invitation = Table(
-    "sent_invitation", metadata, Column("entry", String, primary_key=True)
+    "persistent_set_sent_invitation",
+    metadata,
+    Column("entry", String, primary_key=True),
 )
+
 
 @attr.s
 class CachedEngine:
     engine = attr.ib()
     database_url = attr.ib()
 
+
 CACHED_ENGINE = CachedEngine(None, None)
+
 
 def get_conn():
     global CACHED_ENGINE
     if CACHED_ENGINE.database_url != os.environ["DATABASE_URL"]:
         engine = create_engine(os.environ["DATABASE_URL"])
         CACHED_ENGINE = CachedEngine(engine, os.environ["DATABASE_URL"])
-        metadata.create_all(engine)
+        with engine.connect() as conn:
+            alembic_cfg = alembic.config.Config(
+                Path(__file__).parent / "alembic.ini"
+            )
+            alembic_cfg.attributes["connection"] = conn
+            alembic.command.upgrade(alembic_cfg, "head")
+        # Verify that the final schema matches what we expect
+        with engine.connect() as conn:
+            mc = alembic.migration.MigrationContext.configure(conn)
+            diff = alembic.autogenerate.compare_metadata(mc, metadata)
+            if diff:
+                print("!!! mismatch between db schema and code")
+                pprint.pprint(diff)
+                raise RuntimeError("consistency check failed")
     return CACHED_ENGINE.engine.connect()
 
 
@@ -33,7 +57,11 @@ class SentInvitation:
             #   SELECT EXISTS (SELECT 1 FROM sent_invitation WHERE entry = ?)
             return conn.execute(
                 select(
-                    [exists(select([1]).where(sent_invitation.c.entry == name))]
+                    [
+                        exists(
+                            select([1]).where(sent_invitation.c.entry == name)
+                        )
+                    ]
                 )
             ).scalar()
 
---
+++ snekomatic/migrations/versions/1479437ee1e2_initial.py
@@ -0,0 +1,27 @@
+"""empty message
+
+Revision ID: 1479437ee1e2
+Revises: 
+Create Date: 2020-02-21 12:03:25.898594
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "1479437ee1e2"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "persistent_set_sent_invitation",
+        sa.Column("entry", sa.String, primary_key=True),
+    )
+
+
+def downgrade():
+    op.drop_table("sent_invitation")
